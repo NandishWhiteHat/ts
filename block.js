378a630a0f6ddcb25a96b6ece637547bce9d2852
NandishWhiteHat <81525616+NandishWhiteHat@users.noreply.github.com>
2021-10-22 17:15:06 -0400

    Update block.js

--- block.js
+++ block.js
@@ -1,21 +1,30 @@
 class Block{
-    constructor(x, y, width, height) {
-        var options = {
-                     
-        }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
-        World.add(world, this.body);
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
       }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.visibilty = 255;
+      this.image = loadImage("block.png")
+      World.add(world, this.body);
+    }
       display(){
-        var angle = this.body.angle;
         var pos= this.body.position;
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        rectMode(CENTER);
-        rect(0,0,this.width, this.height);
-        pop();
+      
+        if(this.body.speed < 3){
+          imageMode(CENTER);
+          image(this.image, pos.x,pos.y,this.width, this.height);
+        } else {
+          World.remove(world, this.body);
+          push();
+          this.visibilty-=5;
+          tint(255, this.visibilty);
+          imageMode(CENTER);
+          image(this.image, pos.x,pos.y,this.width, this.height);
+          pop();
       }
+}
 }
